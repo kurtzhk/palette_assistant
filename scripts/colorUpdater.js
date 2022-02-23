18d81eafcb4801c8aa1e6376c7e12124dbd3922e
kurtzhk <kurtzhk@wwu.edu>
2022-02-22 16:21:42 -0800

    ready for individual modification

--- scripts/colorUpdater.js
+++ scripts/colorUpdater.js
@@ -18,10 +18,33 @@ function startup() {
 function updateColor(event) {
     //same shiz
     var palettes = Array.from($("#activepalette").children('input'));
-    var mirrors = Array.from($("#zombiepalette").children('.colorblock'));
+    var basepalette = Array.from($("#basepalette").children('.colorblock'));
+    var pal1 = Array.from($("#modpalette1").children('.colorblock'));
+    var pal2 = Array.from($("#modpalette2").children('.colorblock'));
+    var pal3 = Array.from($("#modpalette3").children('.colorblock'));
 
     //iterate through each one and apply color value to div
-    for(i = 0; i < palettes.length; i++){
-        $(mirrors[i]).css({"backgroundColor":palettes[i].value});
+    for (i = 0; i < palettes.length; i++) {
+        //coloring the pickers
+        $(palettes[i]).css({ "backgroundColor": palettes[i].value });
+        $(basepalette[i]).css({ "backgroundColor": palettes[i].value });
+
+        deuteranopia(palettes, pal1);
+        protanopia(palettes, pal2);
+        tritanopia(palettes, pal3);
     }
+}
+
+function deuteranopia(palettes, pal) {
+    //palettes[i].value should be modified by the matrix multi.
+    //and then applied to the backgound color V
+    $(pal[i]).css({ "backgroundColor": palettes[i].value });
+}
+
+function protanopia(palettes, pal) {
+    $(pal[i]).css({ "backgroundColor": palettes[i].value });
+}
+
+function tritanopia(palettes, pal) {
+    $(pal[i]).css({ "backgroundColor": palettes[i].value });
 }
