1ddb9c26bbead22530dd4ddf207f984feefdf792
NathanNielsen <44419214+NathanNielsen@users.noreply.github.com>
2022-03-05 18:38:02 -0800

    Add files via upload

--- scripts/colorUpdater.js
+++ scripts/colorUpdater.js
@@ -2,43 +2,47 @@ var active1, active2, active3, active4, active5;
 
 window.addEventListener("load", startup, false);
 
-function startup() {
-    //fetching all the input children and arrayifying them
-    var children = Array.from($("#activepalette").children('input'));
+function startup()
+{
+  //fetching all the input children and arrayifying them
+  var children = Array.from($("#activepalette").children('input'));
 
-    //apply some listeners to them
-    children.forEach(picker =>
-        addEventListener("input", updateColor, false),
-        addEventListener("change", updateColor, false));
+  //apply some listeners to them
+  children.forEach(picker =>
+      addEventListener("input", updateColor, false),
+      addEventListener("change", updateColor, false));
 
-    //just to get colors on them on load
-    updateColor();
+  //just to get colors on them on load
+  updateColor();
 }
 
-function updateColor(event) {
-    //same shiz
-    var palettes = Array.from($("#activepalette").children('input'));
-    var basepalette = Array.from($("#basepalette").children('.colorblock'));
-    var pal1 = Array.from($("#modpalette1").children('.colorblock'));
-    var pal2 = Array.from($("#modpalette2").children('.colorblock'));
-    var pal3 = Array.from($("#modpalette3").children('.colorblock'));
+function updateColor(event)
+{
+  //fetching all the input children and arrayifying them
+  var palettes = Array.from($("#activepalette").children('input'));
+  var basepalette = Array.from($("#basepalette").children('.colorblock'));
+  var pal1 = Array.from($("#modpalette1").children('.colorblock'));
+  var pal2 = Array.from($("#modpalette2").children('.colorblock'));
+  var pal3 = Array.from($("#modpalette3").children('.colorblock'));
 
-    //iterate through each one and apply color value to div
-    for (i = 0; i < palettes.length; i++) {
-        //coloring the pickers
-        //but not if it has a lock
-        if (basepalette[i].classList.contains("unlock")) {
-            $(palettes[i]).css({ "backgroundColor": palettes[i].value });
-            $(basepalette[i]).css({ "backgroundColor": palettes[i].value });
-        }
+  //iterate through each one and apply color value to div
+  for (i = 0; i < palettes.length; i++)
+  {
+    //coloring the pickers
+    //but not if it has a lock
+    if (basepalette[i].classList.contains("unlock"))
+    {
+      $(palettes[i]).css({ "backgroundColor": palettes[i].value });
+      $(basepalette[i]).css({ "backgroundColor": palettes[i].value });
     }
+  }
 
-    protanopia(palettes, pal1);
-    deuteranopia(palettes, pal2);
-    tritanopia(palettes, pal3);
+  protanopia(palettes, pal1);
+  deuteranopia(palettes, pal2);
+  tritanopia(palettes, pal3);
 }
 
-function protanopia(palettes, pal) {
+function protanopia(palettes, pal){
     applyColorblind(palettes, pal, [
         [0.367322, 0.860646, -0.227968],
         [0.280085, 0.672501, 0.047413],
@@ -62,53 +66,61 @@ function tritanopia(palettes, pal) {
     ])
 }
 
-function applyColorblind(palettes, pal, colorblindMatrix) {
-    const inputColors = new Array(palettes.length);
-    const outputColors = new Array(palettes.length);
-    var basepalette = Array.from($("#basepalette").children('.colorblock'));
+function applyColorblind(palettes, pal, colorblindMatrix)
+{
+  const inputColors = new Array(palettes.length);
+  const outputColors = new Array(palettes.length);
+  var basepalette = Array.from($("#basepalette").children('.colorblock'));
 
-    for (i = 0; i < palettes.length; i++) {
-        var inputColor = parseInt(palettes[i].value.substring(1), 16);
-        //console.log(inputColor);
-        //console.log(palettes[i].value);
-        inputColors[i] = new Array(3);
-        outputColors[i] = new Array(3);
-        inputColors[i][2] = inputColor % 256;
-        inputColor = inputColor / 256;
-        inputColor = Math.floor(inputColor);
-        inputColors[i][1] = inputColor % 256;
-        inputColor = inputColor / 256;
-        inputColor = Math.floor(inputColor);
-        inputColors[i][0] = inputColor % 256;
-    }
+  for (i = 0; i < palettes.length; i++)
+  {
+    var inputColor = parseInt(palettes[i].value.substring(1), 16);
+    //console.log(inputColor);
+    //console.log(palettes[i].value);
+    inputColors[i] = new Array(3);
+    outputColors[i] = new Array(3);
+    inputColors[i][2] = inputColor % 256;
+    inputColor = inputColor / 256;
+    inputColor = Math.floor(inputColor);
+    inputColors[i][1] = inputColor % 256;
+    inputColor = inputColor / 256;
+    inputColor = Math.floor(inputColor);
+    inputColors[i][0] = inputColor % 256;
+  }
 
-    //console.log(inputColors);
-    for (i = 0; i < palettes.length; i++) {
-        for (j = 0; j < 3; j++) {
-            var colorToOutput = 0;
-            for (k = 0; k < 3; k++) {
-                colorToOutput = colorToOutput + (inputColors[i][k] * colorblindMatrix[k][j]);
-            }
-            colorToOutput = Math.min(Math.max(colorToOutput, 0), 255)
-            outputColors[i][j] = colorToOutput;
-        }
-        var compositeColor = Math.round(outputColors[i][0]);
-        compositeColor *= 256;
-        compositeColor += Math.round(outputColors[i][1]);
-        compositeColor *= 256;
-        compositeColor += Math.round(outputColors[i][2]);
-        var compositeColor = compositeColor.toString(16);
-        outputColors[i] = "#" + compositeColor;
+  //console.log(inputColors);
+  for (i = 0; i < palettes.length; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      var colorToOutput = 0;
+      for (k = 0; k < 3; k++)
+      {
+        colorToOutput = colorToOutput + (inputColors[i][k] * colorblindMatrix[k][j]);
+      }
+      colorToOutput = Math.min(Math.max(colorToOutput, 0), 255)
+      outputColors[i][j] = colorToOutput;
     }
+    var compositeColor = Math.round(outputColors[i][0]);
+    compositeColor *= 256;
+    compositeColor += Math.round(outputColors[i][1]);
+    compositeColor *= 256;
+    compositeColor += Math.round(outputColors[i][2]);
+    var compositeColor = compositeColor.toString(16);
+    outputColors[i] = "#" + compositeColor;
+  }
 
-    for (i = 0; i < palettes.length; i++) {
-        if (basepalette[i].classList.contains("unlock")) {
-            $(pal[i]).css({ "backgroundColor": outputColors[i] });
-        }
+  for (i = 0; i < palettes.length; i++)
+  {
+    if (basepalette[i].classList.contains("unlock"))
+    {
+      $(pal[i]).css({ "backgroundColor": outputColors[i] });
     }
+  }
 }
 
-function generate(scheme) {
+function generate(scheme)
+{
     //should take the color from the lonely color box
     //and the forms input
     //and use it to generate a palette
--- scripts/siteActions.js
+++ scripts/siteActions.js
@@ -1,20 +1,23 @@
-function applyLock(blockIndex) {
+function applyLock(blockIndex)
+{
     var basepalette = Array.from($("#basepalette").children('.colorblock'));
 
     //lock
-    if (basepalette[blockIndex].classList.contains("unlock")) {
-        basepalette[blockIndex].classList.add("lock");
-        basepalette[blockIndex].classList.remove("unlock");
+    if (basepalette[blockIndex].classList.contains("unlock"))
+    {
+      basepalette[blockIndex].classList.add("lock");
+      basepalette[blockIndex].classList.remove("unlock");
 
-        basepalette[blockIndex].innerHTML = "";
-        basepalette[blockIndex].innerHTML = "<img src='images/lock_closed.svg' />";
+      basepalette[blockIndex].innerHTML = "";
+      basepalette[blockIndex].innerHTML = "<img src='images/lock_closed.svg' />";
     }
     //unlock 
-    else if (basepalette[blockIndex].classList.contains("lock")) {
-        basepalette[blockIndex].classList.add("unlock");
-        basepalette[blockIndex].classList.remove("lock");
+    else if (basepalette[blockIndex].classList.contains("lock"))
+    {
+      basepalette[blockIndex].classList.add("unlock");
+      basepalette[blockIndex].classList.remove("lock");
 
-        basepalette[blockIndex].innerHTML = "";
-        basepalette[blockIndex].innerHTML = "<img src='images/lock_open.svg' />";
+      basepalette[blockIndex].innerHTML = "";
+      basepalette[blockIndex].innerHTML = "<img src='images/lock_open.svg' />";
     }
 }
